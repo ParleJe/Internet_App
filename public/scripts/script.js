c01dfb55205140a099c42da2056ecdff0d228258
Robert Kozik <rkozik98@gmail.com>
2020-11-11 15:31:01 +0100

    fix js onClick function

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,31 @@
+function vh(v) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (v * h) / 100;
+  }
+  
+  function vw(v) {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (v * w) / 100;
+  }
+  
+  function vmin(v) {
+    return Math.min(vh(v), vw(v));
+  }
+  
+  function vmax(v) {
+    return Math.max(vh(v), vw(v));
+  }
+
 function showMenu() {
-/*    document.getElementById('navigation-bar').classList.add('slide'); */
-alert("Hello World!");
+
+    var element = document.getElementById('navigation-bar'),
+        style = window.getComputedStyle(element),
+        left = style.getPropertyValue('left');
+        
+    if(left == "0px") {
+        element.style.left = new String(vw(-70)+'px');
+    }else {
+        element.style.left = vw(0);
+    }
+
 }
