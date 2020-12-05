db026c250de4da620dbb7e43bb9fdd360a6e3673
Robert Kozik <rkozik98@gmail.com>
2020-12-05 21:25:44 +0100

    template of changes

---
+++ public/views/profile.php
@@ -0,0 +1,7 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="public/css/profile-stylesheet.css">
+
+    <script src="https://kit.fontawesome.com/a19050df1f.js" crossorigin="anonymous"></script>
+    <title>Create</title>
+</head>
