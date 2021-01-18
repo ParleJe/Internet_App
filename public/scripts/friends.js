6e68a40105cc36e32991c29bb74bc895fbc3a072
Robert Kozik <rkozik98@gmail.com>
2021-01-19 00:51:59 +0100

    add search template in search.php, search.js uses templete to create new HTML nodes

--- public/scripts/friends.js
+++ public/scripts/friends.js
@@ -23,8 +23,11 @@ const appendObject = (object) => {
     const clone = template.content.cloneNode(true);
 
     clone.querySelector('div').id = object.mortal_id;
-    clone.querySelector('img').src = '/public/resources/placeholder.jpg'
-        //object.photo_directory;
+    if(object.photo_directory === undefined) {
+        clone.querySelector('img').src = '/public/resources/placeholder.jpg'
+    } else {
+        clone.querySelector('img').src = object.photo_directory;
+    }
     clone.querySelector('h2').innerHTML = object.name + ' ' + object.surname;
     clone.querySelector('h3').innerHTML = object.nickname;
 
---
+++ public/scripts/search.js
@@ -0,0 +1,29 @@
+import {fetchData, post} from "./fetchAPI.js";
+
+const searchInput = document.querySelector('.search-input');
+const view = $('.content');
+
+document.querySelector('.search-btn').addEventListener('click', async() => {
+    const input = searchInput.value;
+        const json = await fetchData({dataType: 'trip', data: input}, post);
+        display(json);
+
+})
+
+const display = (response) => {
+    response.map(object => appendNode(object));
+}
+
+const appendNode = (object) => {
+    const template = document.querySelector('#search-template');
+    const clone = template.content.cloneNode(true);
+
+    clone.id = 'search-' + object.trip_id;
+    clone.querySelector('a').href = `view?tripId=${object.trip_id}&type=template`;
+    clone.querySelector('img').src = object.photo_directory;
+    clone.querySelector('h2').innerHTML = object.trip_name;
+    clone.querySelector('p').innerHTML = object.description;
+
+    const view = document.querySelector('.content');
+    view.append(clone);
+}
