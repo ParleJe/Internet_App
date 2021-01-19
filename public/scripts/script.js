d1546e624adf4c6c3a68141638907fafbbd6a0fd
Robert Kozik <rkozik98@gmail.com>
2021-01-19 01:11:48 +0100

    fix typo

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,7 +3,7 @@ import {fetchData, put} from "./fetchAPI.js";
 const codeDiv = document.querySelector('#take-part');
 const membersContainer = document.querySelector('#members');
 const codeInput = document.querySelector('#take-part>input');
-const trips = document.querySelectorAll('.trip');
+
 document.querySelector(".fa-plus-circle").addEventListener('click', async() => {
     alert(codeInput.value);
 
@@ -14,14 +14,11 @@ document.querySelector(".fa-plus-circle").addEventListener('click', async() => {
 
 const display = (json) => {
     const template = document.querySelector("#trip-template");
-
     const clone = template.content.cloneNode(true);
+
     clone.querySelector('h4').innerHTML = json.destination;
-
     clone.querySelector('h3').innerHTML = json.trip_name;
-
     clone.querySelector('form>div>input').src = json.photo_directory;
-
     clone.querySelector('form>#id').value = json.trip_id;
 
     membersContainer.insertBefore(clone, codeDiv);
@@ -41,18 +38,3 @@ const addListener = () => {
 }
 
 addListener();
-//create.php
-$("textarea").on("keyup", function() {
-    var limit = $(this).attr("maxlength");
-
-    if (!limit) return;
-
-    if (this.value.length <= limit) return true;
-    else {
-        this.value = this.value.substr(0,limit);
-        return false;
-    }
-})
-
-
-
