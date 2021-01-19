fe9ecb56ce420a222450a45660e9dd7b270f316c
Robert Kozik <rkozik98@gmail.com>
2021-01-19 01:13:20 +0100

    change const view scope to global

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,7 +1,7 @@
 import {fetchData, post} from "./fetchAPI.js";
 
 const searchInput = document.querySelector('.search-input');
-const view = $('.content');
+const view = document.querySelector('.content');
 
 document.querySelector('.search-btn').addEventListener('click', async() => {
     const input = searchInput.value;
@@ -18,12 +18,11 @@ const appendNode = (object) => {
     const template = document.querySelector('#search-template');
     const clone = template.content.cloneNode(true);
 
-    clone.id = 'search-' + object.trip_id;
+    clone.id = `search-${object.trip_id}`;
     clone.querySelector('a').href = `view?id=${object.trip_id}&type=template`;
     clone.querySelector('img').src = object.photo_directory;
     clone.querySelector('h2').innerHTML = object.trip_name;
     clone.querySelector('p').innerHTML = object.description;
 
-    const view = document.querySelector('.content');
     view.append(clone);
 }
