0824d57894cfc80971c56ad38b5512fc8d38ef04
Robert Kozik <rkozik98@gmail.com>
2021-01-19 18:02:44 +0100

    fix warnings, code cleanup

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -8,7 +8,8 @@ class LoginController extends AppController {
 
     public function login() {
         if(!$this->isPost()) {
-            return $this->render('login');
+            $this->render('login');
+            return;
         }
 
         $mail = $_POST["email"];
@@ -17,28 +18,31 @@ class LoginController extends AppController {
         $userDB = new UserRepository();
 
         $user = $userDB->getUser($mail);
-        $user = $user[0];
 
         if ( is_null( $user ) ) {
-            return $this->render('login', ['messages' => ['Wrong mail!']]);
+            $this->render('login', ['messages' => ['Wrong mail!']]);
+            return;
         }
 
         if( ! password_verify( $passwd, $user->getPassword() ) )
         {
-            return $this->render('login', ['messages' => ['Wrong password!']]);
+            $this->render('login', ['messages' => ['Wrong password!']]);
+            return;
         }
 
         if( $user->isLog() ) {
-            return $this->render('login', ['messages' => ['User already logged in']]);
+            $this->render('login', ['messages' => ['User already logged in']]);
+            return;
         }
 
         if(session_start() && $userDB->setUserStatus($user->getMortalId())) {
             $_SESSION['user_id'] = $user->getMortalId();
             $_SESSION['isLoggedIn'] = true;
-            return Routing::run('trips');
+            Routing::run('trips');
+            return;
         }
 
-        return $this->render('login', ['messages' => ['Something went wrong']]);
+        $this->render('login', ['messages' => ['Something went wrong']]);
     }
 
     public function logout () {
@@ -46,35 +50,88 @@ class LoginController extends AppController {
         if( $repo->setUserStatus( $this->getCurrentLoggedID()) ){
             session_unset();
             session_destroy();
-            return Routing::run('');
+            Routing::run('');
+            return;
         }
-        // TODO WHAT IF IT WONT WORK ???
     }
 
     public function registration() {
         if( ! $this->isPost() ) {
-            return $this->render('registration');
+            $this->render('registration');
+            return;
         }
         if($_POST["password"] != $_POST["reentered-password"]) {
-            return $this->render("registration", ['messages' => ["Passwords did not match"]]);
+            $this->render("registration", ['messages' => ["Passwords did not match"]]);
+            return;
         }
 
+        if( !(is_uploaded_file($_FILES['photo']['tmp_name']) || $this->validatePhoto($_FILES['photo'])) ) {
+            $this->render("registration", ['messages' => ["Invalid Photo!"]]);
+            return;
+        }
+
+        $photoDIR = $this->getUploadDirectory($_FILES['photo']);
+        $quote = $_POST["quote"];
+        $login = $_POST["login"]; //can be multiple similar nicknames
         $mail = $_POST["email"];
-        $login = $_POST["login"];
+        if ( !$this->checkEmail($mail) ){
+            $this->render("registration", ['messages' => ["E-mail already registered!"]]);
+            return;
+        }
+
+
         $passwd = password_hash( $_POST["password"], PASSWORD_ARGON2ID, $this->options );
         $newUser = new User([
-            'user_id' => null,
             'mail' => $mail,
             'password' => $passwd,
             'role_id' => User::USER,
-            'nickname' => $login
+            'nickname' => $login,
+            'quote' => $quote,
+            'photo_directory' => $photoDIR
         ]);
 
         $userDB = new UserRepository();
         if( ! $userDB->setUserByTransaction( $newUser ) ) {
-            return $this->render("registration", ['messages' => ["Sorry, we have problem with connection"]]);
+            $this->render("registration", ['messages' => ["Sorry, we have problem with connection"]]);
+            return;
+        }
+        move_uploaded_file(
+            $_FILES['photo']['tmp_name'],
+            $photoDIR
+        );
+
+        $this->render('login'); // You made it! Back to login page
+    }
+
+    /**
+     * @param string $mail
+     * @return bool true if email does not exist, false if email exists in db
+     */
+    private function checkEmail(string $mail):bool {
+        $repo = new UserRepository();
+        $user = $repo->getUser($mail);
+        return is_null($user);
+    }
+
+    //TODO to photoController.php
+    private function validatePhoto(array $file): bool
+    {
+        $MAX_FILE_SIZE =  1024 * 1024;
+        $SUPPORTED_EXTENSIONS  = ['image/png', 'image/jpeg'];
+        if ($file['size'] > $MAX_FILE_SIZE) {
+            $this->messages[] = 'File is too large';
+            return false;
         }
 
-        return $this->render('login'); // You made it! Back to login page
+        if (!isset($file['type']) && !in_array($file['type'], $SUPPORTED_EXTENSIONS)) {
+            $this->messages[] = 'unsupported file type';
+            return false;
+        }
+
+        return true;
+    }
+    private function getUploadDirectory(array $file): ?string {
+        $UPLOAD_DIRECTORY =  '/../public/uploads/';
+        return dirname(__DIR__).$UPLOAD_DIRECTORY.$file['name'];
     }
 }
