5c0bc5fe60a9dff861ec53f755ad810e3cf9d817
Robert Kozik <rkozik98@gmail.com>
2021-01-20 21:51:41 +0100

    remove unused getters and setters

--- src/models/User.php
+++ src/models/User.php
@@ -107,26 +107,6 @@ class User implements JsonSerializable
         $this->role_id = $role_id;
     }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(?string $name): void
-    {
-        $this->name = $name;
-    }
-
-    public function getSurname(): ?string
-    {
-        return $this->surname;
-    }
-
-    public function setSurname(?string $surname): void
-    {
-        $this->surname = $surname;
-    }
-
     public function getNickname(): ?string
     {
         return $this->nickname;
